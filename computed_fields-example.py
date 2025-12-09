cfbad5884bb6b59a3025501865719e18c30e621b
Varshith Mohan <95514418+varshith-mohan@users.noreply.github.com>
2025-12-09 12:22:58 +0530

    Add files via upload

---
+++ computed_fields-example.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, EmailStr, computed_field
+from typing import List, Dict
+
+class Patient(BaseModel):
+
+    name: str
+    email: EmailStr
+    age: int
+    weight: float # kg
+    height: float # mtr
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+
+
+
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print('BMI', patient.bmi)
+    print('updated')
+
+patient_info = {'name':'nitish', 'email':'abc@icici.com', 'age': '65', 'weight': 75.2, 'height': 1.72, 'married': True, 'allergies': ['pollen', 'dust'], 'contact_details':{'phone':'2353462', 'emergency':'235236'}}
+
+patient1 = Patient(**patient_info) 
+
+update_patient_data(patient1)
