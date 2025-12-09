ae59151fe4a82d941f34bc9cc5e5f7ca05a3ab87
Varshith Mohan <95514418+varshith-mohan@users.noreply.github.com>
2025-12-09 12:42:38 +0530

    Add files via upload

---
+++ nested_models-example.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+
+    city: str
+    state: str
+    pin: str
+
+class Patient(BaseModel):
+
+    name: str
+    gender: str
+    age: int
+    address: Address
+
+address_dict = {'city': 'gurgaon', 'state': 'haryana', 'pin': '122001'}
+
+address1 = Address(**address_dict)
+
+patient_dict = {'name': 'nitish', 'gender': 'male', 'age': 35, 'address': address1}
+
+patient1 = Patient(**patient_dict)
+
+temp = patient1.model_dump(include=)
+
+print(type(temp))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Better organization of related data (e.g., vitals, address, insurance)
+
+# Reusability: Use Vitals in multiple models (e.g., Patient, MedicalRecord)
+
+# Readability: Easier for developers and API consumers to understand
+
+# Validation: Nested models are validated automatically—no extra work needed
